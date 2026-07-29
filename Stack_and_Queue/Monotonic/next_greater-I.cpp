baeb69e10d01bd04429d10ba3fdb575721604ee4
SANAD BANSAL <sanadwork240@gmail.com>
2026-07-29 10:44:18 +0530

    next_greater-I

---
+++ Stack_and_Queue/Monotonic/next_greater-I.cpp
@@ -0,0 +1,17 @@
+//  here the question is exactly same as the normal next greater lement but the only difference is that the circular elemnt can also be choose so to attempt this we will hypothetiically assume that there is double array of length n 2 10 12 1 11  -- 2 10 12 1 11 2 10 12 1 11 now the solution is exact same but we will only incluyde the nge insertion from 11 index 5 while starting from the right end alse we have to add the index=j%n
+
+ stack<int>st;
+  
+ for(i=2n-1 ->0){
+    while ( --- && st.top()<a[i%n]){
+
+    }
+    if(i<n){
+--
+--
+    }
+    st.push(a[i%n]);
+ }
+
+
+
