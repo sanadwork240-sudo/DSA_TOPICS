db4369ec7b5292e0bfb2967b8e1f3dfb077763ef
SANAD BANSAL <sanadwork240@gmail.com>
2026-08-09 23:41:33 +0530

    structure

---
+++ Stack_and_Queue/Hard/subarray_window_maximum.cpp
@@ -0,0 +1,10 @@
+//  the aim is to find the maximum i n the subarray window of size k can be three eg 
+// 1 3 -1 -3 5  3 2 1 6
+// 1 brute force is to calculate every subarray of size k minimum 
+//  for(int i=0->n-k){
+    
+//     for(j=i->>i+k-1){
+//         maxi=max(maxi,a[j]);
+//     }
+//     ls.add(maxi);
+
