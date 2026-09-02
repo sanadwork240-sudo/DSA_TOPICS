365386a2f4376bf9840454594a8420861d29f4d0
SANAD BANSAL <sanadwork240@gmail.com>
2026-09-03 00:44:03 +0530

    dfs_traversal

--- TREES/Traversal/DFS.cpp
+++ TREES/Traversal/DFS.cpp
@@ -1,1 +1,40 @@
 // inorder ,oreorder and postorder these are all the types of the dfs 
+
+
+struct node{
+    int data;
+    node*left;
+    node*right;
+    node(int val){
+        data=val;
+        left=right=nullptr;
+    }
+} 
+
+// inorder code left root right
+void inorder(node*temp){
+    if(temp==null)return;
+    inorder(temp->left);
+    cout<<temp->data;
+    inorder(temp->right);
+}
+
+// preorder root left right
+void preorder(node*temp){
+    if(temp==null)return;
+    
+    cout<<temp->data;
+    preorder(temp->left);
+    preorder(temp->right);
+}
+
+// postorder
+void preorder(node*temp){
+    if(temp==null)return;
+    
+    preorder(temp->left);
+    preorder(temp->right);
+    cout<<temp->data;
+
+}
+
