3dd44051381ee90d2c4786b8c6f33a18eeb3b610
SANAD BANSAL <sanadwork240@gmail.com>
2026-09-07 19:35:15 +0530

    tree

---
+++ TREES/Iterative_postorder.cpp
@@ -0,0 +1,6 @@
+//  here botn the two stack traversal and the single stack traversal technique will be used in order to solve the solution and hence the intution will be understand using the solution 
+
+
+
+//  first one is the two stack solution for the postorder solution in which the root is taken first enteres into the stack 1 and then pop and added to the stack 2 and the left and right if present then entered into the stack 1 and the whole processs is repeated till stack n1 is empty . then the answer is take and the st2.pop is used to store the answer.
+// time complexity and the space complkexity are  O(n) and O(2n) respectively .
